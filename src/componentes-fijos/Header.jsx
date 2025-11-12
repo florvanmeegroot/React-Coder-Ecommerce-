e44c4b403b4b773bab65491d39d1aa016fd48377
florvanmeegroot <florvanmeegroot@gmail.com>
2025-11-12 15:55:38 -0300

    header

---
+++ src/componentes-fijos/Header.jsx
@@ -0,0 +1,19 @@
+function Header() {
+  return (
+    <header className="header">
+      <img
+        src="./src/assets/img/icono-tienda.png"
+        alt="Logo Tiendita"
+        className="logo"
+      />
+      <h1 className="tittle-header">La Tiendita</h1>
+      <nav className="nav-header">
+        <a href="#">Inicio</a>
+        <a href="#">Categorias</a>
+        <a href="#">Contacto</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
